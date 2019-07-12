9a76832c56a8e1ca75010e29e166e79f5732ba83
Cilas Amos <cilas.acms@gmail.com>
2019-07-12 20:13:01 -0300

    feature model comission plan

--- codenation/televendas/models.py
+++ codenation/televendas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Model Plano de Comissão
+class ComissionPlan(models.Model):
+    description = models.CharField("Descrição", max_length=200)
+    lower_percentage = models.FloatField("Menor Porcentagem")
+    min_value = models.DecimalField("Valor Mínimo", max_digits=8, decimal_places=2)
+    upper_percentage = models.FloatField("Maior Porcentagem")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name_plural = "Planos de Comissões"
+
+    def __str__(self):
+        return '{}'.format(self.description)
---
+++ codenation/televendas/tests/factories.py
@@ -0,0 +1,12 @@
+import factory
+from faker import Faker
+from ..models import ComissionPlan
+
+fake = Faker('pt_BR')
+
+class ComissionPlanFactory(factory.DjangoModelFactory):
+    lower_percentage = fake.pyfloat(left_digits=3, right_digits=2, positive=True, min_value=0, max_value=5)
+    min_value = fake.pydecimal(left_digits=None, right_digits=2, positive=True, min_value=1000, max_value=10000)
+    upper_percentage = fake.pyfloat(left_digits=3, right_digits=2, positive=True, min_value=0, max_value=10)
+    class Meta:
+        model = ComissionPlan
