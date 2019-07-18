226eed3e54e93c4f48495de0fe0ee57ce70b1404
Alexandre Müller Júnior <alexandremj@protonmail.com>
2019-07-18 01:02:06 -0300

    Create Seller model

--- codenation/televendas/models.py
+++ codenation/televendas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Seller(models.Model):
+    # parece suficiente para a grande maioria dos casos
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
+
+    # precisamos que o campo se comporte como um inteiro?
+    phone_number = models.CharField(max_length=15)
+    age = models.CharField(max_length=3)
+    cpf = models.CharField(max_length=11, primary_key=True)
+    comission_plan = models.CharField(max_length=2)
+
+    # TODO: verificar possibilidade de validacao programatica do formato
+    # deste email
+    email = models.CharField(max_length=50)
