0dd94f252466fe4c631428784cf681c2cc5c30c1
Alexandre Müller Júnior <alexandremj@protonmail.com>
2019-07-23 17:39:32 -0300

    Adiciona recomendacoes e facilita o merge com master

--- codenation/televendas/models.py
+++ codenation/televendas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Model Plano de Comissão
 class ComissionPlan(models.Model):
@@ -15,3 +16,39 @@ class ComissionPlan(models.Model):
 
     def __str__(self):
         return '{}'.format(self.description)
+
+# Model Vendedor
+class Seller(models.Model):
+    # parece suficiente para a grande maioria dos casos
+    name = models.CharField(max_length=50)
+    address = models.TextField("Endereço", help_text="Address of Seller")
+
+    # precisamos que o campo se comporte como um inteiro?
+    phone_number = models.CharField(max_length=15)
+    birthday = models.DateField("Data de Nascimento", help_text="Birthday of Seller")
+    cpf = models.CharField(max_length=11, primary_key=True)
+    comission_plan = models.ForeignKey(
+                                        ComissionPlan,
+                                        related_name='sellers',
+                                        on_delete=models.CASCADE,
+                                        verbose_name="Plano de Comissões"
+                                        )
+
+    # TODO: verificar possibilidade de validacao programatica do formato
+    # deste email
+    email = models.EmailField(max_length=254, help_text="Email of Seller")
+
+    # data criacao e atualizacao
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    # Retorna a idade do vendedor
+    @property
+    def age(self):
+        birthday = datetime.datetime.strptime(str(self.birthday), '%Y-%m-%d')
+        today = datetime.date.today()
+        return today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
+
+    def __str__(self):
+        return self.name
+
