ea034cb1119ae02495247517355358f859d3e0d5
Cilas Amos <cilas.acms@gmail.com>
2019-07-24 21:01:05 -0300

    add core api to project

--- televendas/urls.py
+++ televendas/urls.py
@@ -1,10 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views.comission_plan import ComissionPlanViewSet
+from rest_framework.documentation import include_docs_urls
+
 
 router = routers.DefaultRouter()
 router.register(r'comissions', ComissionPlanViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include_docs_urls(title='Televendas API')),
+    path('/api/v1/', include(router.urls))
+
 ]
