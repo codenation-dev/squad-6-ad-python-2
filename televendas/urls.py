032e888c5a8b57b6c56643cbf8bec117a55c32c7
Cilas Amos <cilas.acms@gmail.com>
2019-07-29 09:44:11 -0300

    intial check_comission improviments

--- televendas/urls.py
+++ televendas/urls.py
@@ -3,10 +3,9 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views.comission_plan import ComissionPlanViewSet
-from .views.sale import SaleViewSet
+from .views.check_comission import CheckComission
 from .views.seller import SellerViewSet
 from .views.sale import SaleViewSet
-from .views.check_comission import check_comission
 from rest_framework.documentation import include_docs_urls
 
 router = routers.DefaultRouter()
@@ -17,5 +16,5 @@ router.register(r'sales', SaleViewSet)
 urlpatterns = [
     path('', include_docs_urls(title='Televendas API')),
     path('api/v1/', include(router.urls)),
-    path('api/v1/check-comission/', check_comission),
+    path('api/v1/check-comission/', CheckComission.as_view()),
 ]
--- televendas/views/check_comission.py
+++ televendas/views/check_comission.py
@@ -1,41 +1,33 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from televendas.models.sale import Sale
+from televendas.models.seller import Seller
+from televendas.serializers.check_comission import CheckComissionSerializer
 
-
-@api_view(['POST'])
-def check_comission(request):
+class CheckComission(APIView):
     """
     post:
     Check if amount sale is great than avarage of the last five months.
     """
-    if isinstance(request.data['seller'], int):
-        seller = Seller.objects.get(pk=request.data['seller'])
-        sales = Sale.objects.filter(seller_id=seller.id).values_list('amount', flat=True).order_by('-month')[:5]
-        if len(sales) > 0:            
-            sum = 0.0
-            notify = False
-            for idx, val in enumerate(sorted(sales), 1):
-                sum +=  float(val) * float(idx)
-            avg = sum/len(sales)
-            minimum_amount = avg - (avg * 10 / 100)
-            try:
-                if isinstance(request.data['amount'], float):
-                    if minimum_amount > request.data['amount']:
-                        notify = True
-                        try:
-                            send_mail(
-                                'Notification about sales',
-                                'Your sales are below average',
-                                'notify@televendas.local',
-                                [seller.email],
-                                fail_silently=False,
-                            )
-                        except Exception as e:
-                            pass
-                    else:
-                        notify = False
+    def post(self, request, format=None):
+        serializer = CheckComissionSerializer(data=request.data)
+        if serializer.is_valid():
+            seller = get_object_or_404(Seller, pk=request.data['seller'])
+            sales = Sale.objects.filter(seller_id=seller.pk).values_list('amount', flat=True).order_by('-month')[:5]
+            if len(sales) > 0:            
+                sum = 0.0
+                notify = False
+                for idx, val in enumerate(sorted(sales), 1):
+                    sum +=  float(val) * float(idx)
+                avg = sum/len(sales)
+                minimum_amount = avg - (avg * 10 / 100)
+                if minimum_amount > request.data['amount']:
+                    notify = True                            
                 else:
-                    return Response({'error': 'amount must be float type'}, status=status.HTTP_400_BAD_REQUEST)
-            except Exception as e:
-                pass
-            return Response({'notify': notify})
+                    notify = False
+                return Response({'notify': notify}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            
+           
