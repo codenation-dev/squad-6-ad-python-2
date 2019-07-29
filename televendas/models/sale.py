0ed7c7a7625d193ddd0f648f5c462e29d0bbb67e
the-bmb <bruna_braga@unifei.edu.br>
2019-07-28 23:53:35 -0300

    [FIX] auto calculate comission

--- televendas/models/sale.py
+++ televendas/models/sale.py
@@ -24,15 +24,22 @@ class Sale(models.Model):
         Seller, on_delete=models.CASCADE, verbose_name="CPF")
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     month = models.CharField(choices=months, max_length=1)
-    comission = models.DecimalField(max_digits=8, decimal_places=2)
-    
-    def save(self, *args, **kwargs):
+    comission = models.DecimalField(
+        max_digits=8, decimal_places=2, editable=False)
+
+    @property
+    def get_comission(self):
         comission_plan = self.seller.comission_plan
         if self.amount < comission_plan.min_value:
-            self.comission =\
+            comission =\
                 (comission_plan.lower_percentage/100)*float(self.amount)
-        self.comission =\
-            (comission_plan.upper_percentage/100)*float(self.amount)
+        else:
+            comission =\
+                (comission_plan.upper_percentage/100)*float(self.amount)
+        return comission
+
+    def save(self, *args, **kwargs):
+        self.comission = self.get_comission
         super(Sale, self).save(*args, **kwargs)
 
     class Meta:
